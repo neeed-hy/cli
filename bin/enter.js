c44231d8f760e23f521bf466f6a1cf755f56a7d4
1 <1>
2022-10-14 08:37:09 +0800

    feat: 🎸 初始化

--- bin/enter.js
+++ bin/enter.js
@@ -1,2 +1,10 @@
 #! /usr/bin/env node
-console.log("hello demo");
+const program = require('commander')
+
+program.name('demo').usage(`<command> [option]`).version(`1.0.0`)
+
+// 解析用户执行时输入的参数
+// process.argv 是 nodejs 提供的属性
+// npm run server --port 3000
+// 后面的 --port 3000 就是用户输入的参数
+program.parse(process.argv)
