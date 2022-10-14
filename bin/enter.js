14a9fe543110727e90171290c4ad971bc847c548
1 <1>
2022-10-14 08:39:29 +0800

    feat: 🎸 辅助包

--- bin/enter.js
+++ bin/enter.js
@@ -1,5 +1,15 @@
 #! /usr/bin/env node
 const program = require('commander')
+const chalk = require('chalk')
+console.log(`hello ${chalk.blue('world')}`)
+console.log(chalk.blue.bgRed.bold('Hello world!'))
+console.log(
+  chalk.green(
+    'I am a green line ' +
+      chalk.blue.underline.bold('with a blue substring') +
+      ' that becomes green again!'
+  )
+)
 
 program.name('demo').usage(`<command> [option]`).version(`1.0.0`)
 
