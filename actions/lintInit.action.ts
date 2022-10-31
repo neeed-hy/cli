3468921c608bc679cf38767d6c20c9afe1b158f4
1 <1>
2022-10-31 16:30:55 +0800

    feat: 🎸 可以进行eslint的安装

--- actions/lintInit.action.ts
+++ actions/lintInit.action.ts
@@ -1,7 +1,9 @@
 import * as inquirer from 'inquirer'
 import type { Question } from 'inquirer'
-import { generateMultiSelect, generateSingleSelect } from '../lib/question'
-import { exec } from 'shelljs'
+import { generateSingleSelect } from '../lib/question'
+import * as chalk from 'chalk'
+import { outputFileSync } from 'fs-extra'
+
 import {
   packageCommand,
   PackageManager,
@@ -9,14 +11,15 @@ import {
 } from '../lib/packageManager'
 
 enum ELintTools {
-  eslint = 'eslint + prettier',
-  husky = 'husky'
+  eslint = 'eslint',
+  husky = 'husky',
+  prettier = 'prettier'
 }
 
-const lintTools = [ELintTools.eslint, ELintTools.husky]
+const lintTools = [ELintTools.eslint, ELintTools.husky, ELintTools.prettier]
 
 interface IAnswer {
-  type: ELintTools[]
+  type: ELintTools
   packageManager: PackageManager
 }
 
@@ -24,10 +27,54 @@ export async function lintInit() {
   const prompt = inquirer.createPromptModule()
   const questions: Question[] = [
     generateSingleSelect('packageManager', '请选择包管理器', packageManagers),
-    generateMultiSelect('type', '请选择你要初始化的工具', lintTools)
+    generateSingleSelect('type', '请选择你要初始化的工具', lintTools)
   ]
   const answer = await prompt<IAnswer>(questions)
-  const { packageManager } = answer
-  const command = packageCommand(packageManager)
-  exec(command.version())
+  const { type, packageManager } = answer
+  switch (type) {
+    case ELintTools.eslint:
+      installEslint(packageManager)
+      break
+    case ELintTools.prettier:
+      installPrettier(packageManager)
+      break
+    case ELintTools.husky:
+      addHusky(packageManager)
+      break
+    default:
+      throw new Error('工具选择错误')
+  }
+}
+
+/**
+ * 安装eslint
+ * @param thePackage 包管理器
+ */
+function installEslint(thePackage: PackageManager) {
+  const command = packageCommand(thePackage)
+  command.addDev(ELintTools.eslint)
+  outputFileSync('.eslintignore', '/node_modules')
+  console.log('---')
+  console.log('eslint 安装完毕,请执行:')
+  console.log(chalk.green.bold('npm init @eslint/config'))
+  console.log('进行具体的eslint配置。')
+  console.log()
+}
+
+/**
+ * 安装Prettier
+ * @param thePackage 包管理器
+ */
+function installPrettier(thePackage: PackageManager) {
+  const command = packageCommand(thePackage)
+  command.addDev(ELintTools.prettier)
+}
+
+/**
+ * 安装Husky
+ * @param thePackage 包管理器
+ */
+function addHusky(thePackage: PackageManager) {
+  const command = packageCommand(thePackage)
+  console.log(command.version())
 }
--- lib/packageManager.ts
+++ lib/packageManager.ts
@@ -1,3 +1,4 @@
+import { exec } from 'shelljs'
 /**
  * 包管理器
  */
@@ -23,62 +24,69 @@ export interface PackageManagerCommands {
   /**
    * 安装
    */
-  install: () => string
+  install: () => void
   /**
    * 添加到dependencies
    */
-  add: (pkg: string | string[]) => string
+  add: (pkg: string | string[]) => void
   /**
    * 添加到devDependencies
    */
-  addDev: (pkg: string | string[]) => string
+  addDev: (pkg: string | string[]) => void
   /**
    * 更新
    */
-  update: (pkg: string) => string
+  update: (pkg: string) => void
   /**
    * 去除
    */
-  remove: (pkg: string) => string
+  remove: (pkg: string) => void
   /**
    * 版本，可用于环境校验
    */
   version: () => string
+  /**
+   * 直接执行某些语句
+   */
+  exec: (shell: string) => void
 }
 
 /**
  * npm的命令
  */
 const npmCommand: PackageManagerCommands = {
-  install: () => 'npm install',
-  add: (pkg: string | string[]) => `npm install ${pkg} --save`,
-  addDev: (pkg: string | string[]) => `npm install ${pkg} --save-dev`,
-  update: (pkg: string | string[]) => `npm update ${pkg}`,
-  remove: (pkg: string | string[]) => `npm uninstall ${pkg}`,
-  version: () => 'npm -v'
+  install: () => exec('npm install'),
+  add: (pkg: string | string[]) => exec(`npm install ${pkg} --save`),
+  addDev: (pkg: string | string[]) => exec(`npm install ${pkg} --save-dev`),
+  update: (pkg: string | string[]) => exec(`npm update ${pkg}`),
+  remove: (pkg: string | string[]) => exec(`npm uninstall ${pkg}`),
+  version: () => exec('npm -v'),
+  exec: (command: string) => exec(command)
 }
 
 /**
  * yarn的命令
  */
 const yarnCommand: PackageManagerCommands = {
-  install: () => 'yarn',
-  add: (pkg: string | string[]) => `yarn add ${pkg}`,
-  addDev: (pkg: string | string[]) => `yarn add ${pkg} --dev`,
-  update: (pkg: string | string[]) => `yarn upgrade ${pkg}`,
-  remove: (pkg: string | string[]) => `yarn remove ${pkg}`,
-  version: () => 'yarn --version'
+  install: () => exec('yarn'),
+  add: (pkg: string | string[]) => exec(`yarn add ${pkg}`),
+  addDev: (pkg: string | string[]) => exec(`yarn add ${pkg} --dev`),
+  update: (pkg: string | string[]) => exec(`yarn upgrade ${pkg}`),
+  remove: (pkg: string | string[]) => exec(`yarn remove ${pkg}`),
+  version: () => exec('yarn --version'),
+  exec: (command: string) => exec(command)
 }
 /**
  * pnpm的命令
  */
 const pnpmCommand: PackageManagerCommands = {
-  install: () => 'pnpm install',
-  add: (pkg: string | string[]) => `pnpm add ${pkg}`,
-  addDev: (pkg: string | string[]) => `pnpm add ${pkg} --save-dev`,
-  update: (pkg: string | string[]) => `pnpm update ${pkg}`,
-  remove: (pkg: string | string[]) => `pnpm remove ${pkg}`,
-  version: () => 'pnpm -v'
+  install: () => exec('pnpm install'),
+  add: (pkg: string | string[]) => exec(`pnpm add ${pkg}`),
+  addDev: (pkg: string | string[]) => exec(`pnpm add ${pkg} --save-dev`),
+  update: (pkg: string | string[]) => exec(`pnpm update ${pkg}`),
+  remove: (pkg: string | string[]) => exec(`pnpm remove ${pkg}`),
+  version: () => exec('pnpm -v'),
+  exec: (command: string) => exec(command)
 }
 
 /**
@@ -95,7 +103,6 @@ export const packageCommand = (thePackage: PackageManager) => {
     case PackageManager.pnpm:
       return pnpmCommand
     default:
-      // 默认使用pnpm
-      return pnpmCommand
+      throw new Error('包管理器选择错误')
   }
 }
